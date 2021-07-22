1d9901b99685d2831aad2f32ebec3870ad7e92ad
Moses Chawawa <moseschawawa@gmail.com>
2021-07-22 10:43:41 +0200

    Can now parse multiple data frames

--- flourish_reports/views/enrolment/enrolment_reports.py
+++ flourish_reports/views/enrolment/enrolment_reports.py
@@ -1,6 +1,7 @@
 from collections import Counter
 from collections import OrderedDict
 
+import pandas
 from django.apps import apps as django_apps
 
 import pandas as pd
@@ -24,7 +25,6 @@ from flourish_caregiver.models import CaregiverChildConsent
 
 
 class EnrolmentReportMixin:
-
     child_consents_cls = django_apps.get_model('flourish_caregiver.caregiverchildconsent')
     maternal_dataset_cls = django_apps.get_model('flourish_caregiver.maternaldataset')
     child_dataset_cls = django_apps.get_model('flourish_child.childdataset')
@@ -34,12 +34,12 @@ class EnrolmentReportMixin:
         """
         if start_date and end_date:
             consents = CaregiverChildConsent.objects.filter(
-                created__date__gte=start_date,
-                created__date__lte=end_date).values_list(
-                    'cohort', flat=True)
+                created__gte=start_date,
+                created__lte=end_date).values_list(
+                'cohort', flat=True)
         else:
             consents = CaregiverChildConsent.objects.all().values_list(
-                    'cohort', flat=True)
+                'cohort', flat=True)
         cohorts = Counter(consents)
         report = {}
         c_dict = {
@@ -50,7 +50,7 @@ class EnrolmentReportMixin:
             'cohort_b_sec': 'Cohort B Secondary Aims',
             'cohort_c_sec': 'Cohort C Secondary Aims',
             'cohort_pool': 'Cohort Pool'
-            }
+        }
         for key, value in cohorts.items():
             if key:
                 new_key = c_dict[key]
@@ -62,11 +62,11 @@ class EnrolmentReportMixin:
         """
         cohort_a_identifiers = self.child_consents_cls.objects.filter(
             cohort='cohort_a').values_list(
-                'subject_consent__screening_identifier')
+            'subject_consent__screening_identifier')
 
         study_maternal_identifiers = self.maternal_dataset_cls.objects.values_list(
             'study_maternal_identifier', flat=True).filter(
-                screening_identifier__in=cohort_a_identifiers)
+            screening_identifier__in=cohort_a_identifiers)
 
         heu_count = self.child_dataset_cls.objects.filter(
             study_maternal_identifier__in=study_maternal_identifiers,
@@ -83,7 +83,7 @@ class EnrolmentReportMixin:
             'preg_woman': ante_enrol_count,
             'HEU': heu_count,
             'HUU': huu_count
-            }
+        }
         return cohort_a_dict
 
     def cohort_b(self, start_date=None, end_date=None):
@@ -92,11 +92,11 @@ class EnrolmentReportMixin:
 
         cohort_a_identifiers = self.child_consents_cls.objects.filter(
             cohort='cohort_b').values_list(
-                'subject_consent__screening_identifier')
+            'subject_consent__screening_identifier')
 
         study_maternal_identifiers = self.maternal_dataset_cls.objects.values_list(
             'study_maternal_identifier', flat=True).filter(
-                screening_identifier__in=cohort_a_identifiers)
+            screening_identifier__in=cohort_a_identifiers)
 
         preg_efv_count = self.maternal_dataset_cls.objects.filter(
             study_maternal_identifier__in=study_maternal_identifiers,
@@ -114,7 +114,7 @@ class EnrolmentReportMixin:
             'EFV': preg_efv_count,
             'DTG': preg_dtg_count,
             'HIV-Preg': hiv_preg_count
-            }
+        }
         return cohort_b_dict
 
     def cohort_c(self, start_date=None, end_date=None):
@@ -123,11 +123,11 @@ class EnrolmentReportMixin:
 
         cohort_c_identifiers = self.child_consents_cls.objects.filter(
             cohort='cohort_b').values_list(
-                'subject_consent__screening_identifier')
+            'subject_consent__screening_identifier')
 
         study_maternal_identifiers = self.maternal_dataset_cls.objects.values_list(
             'study_maternal_identifier', flat=True).filter(
-                screening_identifier__in=cohort_c_identifiers)
+            screening_identifier__in=cohort_c_identifiers)
 
         huu_count = self.child_dataset_cls.objects.filter(
             study_maternal_identifier__in=study_maternal_identifiers,
@@ -140,7 +140,7 @@ class EnrolmentReportMixin:
         cohort_c_dict = {
             'HUU': huu_count,
             'PI': preg_pi_count,
-            }
+        }
         return cohort_c_dict
 
     def sec_aims(self, start_date=None, end_date=None):
@@ -148,11 +148,11 @@ class EnrolmentReportMixin:
         """
         cohort_sec_identifiers = self.child_consents_cls.objects.filter(
             cohort__icontains='sec').values_list(
-                'subject_consent__screening_identifier')
+            'subject_consent__screening_identifier')
 
         study_maternal_identifiers = self.maternal_dataset_cls.objects.values_list(
             'study_maternal_identifier', flat=True).filter(
-                screening_identifier__in=cohort_sec_identifiers)
+            screening_identifier__in=cohort_sec_identifiers)
 
         hiv_preg_count = self.maternal_dataset_cls.objects.filter(
             study_maternal_identifier__in=study_maternal_identifiers,
@@ -165,15 +165,14 @@ class EnrolmentReportMixin:
         cohort_sec_dict = {
             'WLHIV': hiv_preg_count,
             'HIV -': hiv_neg_preg_count
-            }
+        }
         return cohort_sec_dict
 
 
 class EnrolmentReportView(
-        DownloadReportMixin, EnrolmentReportMixin,
-        EdcBaseViewMixin, NavbarViewMixin,
-        TemplateView, FormView):
-
+    DownloadReportMixin, EnrolmentReportMixin,
+    EdcBaseViewMixin, NavbarViewMixin,
+    TemplateView, FormView):
     form_class = EnrolmentReportForm
     template_name = 'flourish_reports/enrolment_reports.html'
     navbar_name = 'flourish_reports'
@@ -186,30 +185,71 @@ class EnrolmentReportView(
         if form.is_valid():
             start_date = form.data['start_date']
             end_date = form.data['end_date']
-            data = []
-            cohort_report = self.cohort_report(
+
+            cohort_report = self.all_cohort_report(
                 start_date=start_date,
                 end_date=end_date)
+            df1 = pandas.DataFrame(cohort_report, index=[0])
+
             cohort_a = self.cohort_a(
                 start_date=start_date,
                 end_date=end_date)
+            df2 = pandas.DataFrame(cohort_a, index=[1])
+
             cohort_b = self.cohort_b(
                 start_date=start_date,
                 end_date=end_date)
+            df3 = pandas.DataFrame(cohort_b, index=[2])
+
             cohort_c = self.cohort_c(
                 start_date=start_date,
                 end_date=end_date)
+            df4 = pandas.DataFrame(cohort_c, index=[3])
+
             sec_aims = self.sec_aims(
                 start_date=start_date,
                 end_date=end_date)
+            df5 = pandas.DataFrame(sec_aims, index=[4])
+
+            df = [
+                df1,
+                df2,
+                df3,
+                df4,
+                df5
+            ]
+
+            # data.extend([cohort_report, cohort_a, cohort_b, cohort_c, sec_aims])
+
             if 'rdownload_report' in self.request.POST:
                 self.download_data(
-                    description='Enrolment Report',
+                    description='Cohort Report',
                     start_date=start_date, end_date=end_date,
                     report_type='enrolment_reports',
-                    df=pd.DataFrame(data))
+                    # df=pd.DataFrame.from_dict(data, orient='index').fillna(0).transpose())
+                    df=df)
+
+                # self.download_data(
+                #     description='Cohort A Report',
+                #     start_date=start_date, end_date=end_date,
+                #     report_type='enrolment_reports',
+                #     df=pd.DataFrame([cohort_a]))
+                #
+                # self.download_data(
+                #     description='Cohort B Report',
+                #     start_date=start_date, end_date=end_date,
+                #     report_type='enrolment_reports',
+                #     df=pd.DataFrame([cohort_b]))
+                #
+                # self.download_data(
+                #     description='Cohort C Report',
+                #     start_date=start_date, end_date=end_date,
+                #     report_type='enrolment_reports',
+                #     df=pd.DataFrame([cohort_c]))
+
             enrolment_downloads = ExportFile.objects.filter(
-                description='Enrolment Report').order_by('uploaded_at')
+                description='enrolment_reports').order_by('uploaded_at')
+
             context = self.get_context_data(**self.kwargs)
             context.update(
                 enrolment_downloads=enrolment_downloads,
@@ -238,7 +278,7 @@ class EnrolmentReportView(
             cohort_a=cohort_a,
             cohort_b=cohort_b,
             cohort_c=cohort_c,
-            sec_aims=sec_aims,)
+            sec_aims=sec_aims, )
         return context
 
     @method_decorator(login_required)
