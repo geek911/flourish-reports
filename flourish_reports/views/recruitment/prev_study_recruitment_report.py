48303991c337af5a09c4d254483dafe9ab57bd67
Itumeleng Mosweu <imosweuk@gmail.com>
2021-12-07 12:04:57 +0200

    refactor recruitment reports

--- flourish_reports/views/recruitment/prev_study_recruitment_report.py
+++ flourish_reports/views/recruitment/prev_study_recruitment_report.py
@@ -17,56 +17,49 @@ def merge(lst1, lst2):
 
 class PrevStudyRecruitmentReportMixin:
 
-    def total_partici_onworklist(self, prev_study=None, start_date=None, end_date=None):
-        prev_studies = [
+    prev_studies = [
             'Mpepu',
             'Mma Bana',
             'Mashi',
             'Tshilo Dikotla',
             'Tshipidi']
+
+    def total_partici_onworklist(self, prev_study=None, start_date=None, end_date=None):
+
         qs = WorkList.objects.all()
         if prev_study:
-            if prev_study == '-----':
-                qs = WorkList.objects.filter(
+            qs = qs.filter(
                     created__range=[start_date, end_date],)
-            else:
-                qs = WorkList.objects.filter(
-                    prev_study=prev_study,
-                    created__range=[start_date, end_date],)
+            if prev_study != '-----':
+                qs = qs.filter(
+                    prev_study=prev_study)
 
         df = read_frame(qs, fieldnames=['prev_study', 'study_maternal_identifier'])
 
         df = df.drop_duplicates(subset=['study_maternal_identifier'])
 
         prev_study_list = []
-        for prev_study in prev_studies:
+        for prev_study in self.prev_studies:
             df_prev = df[df['prev_study'] == prev_study]
             prev_study_list.append([prev_study, df_prev[df_prev.columns[0]].count()])
         return prev_study_list
 
-    def total_previous_study_participents(self, prev_study=None, start_date=None, end_date=None):
-        prev_studies = [
-            'Mpepu',
-            'Mma Bana',
-            'Mashi',
-            'Tshilo Dikotla',
-            'Tshipidi']
+    def total_previous_study_participents(self, prev_study=None, start_date=None,
+                                          end_date=None):
+
         qs = MaternalDataset.objects.all()
         if prev_study:
-            if prev_study == '-----':
-                qs = MaternalDataset.objects.filter(
-                    created__range=[start_date, end_date],)
-            else:
-                qs = MaternalDataset.objects.filter(
-                    protocol=prev_study,
-                    created__range=[start_date, end_date],)
+            qs = qs.filter(created__range=[start_date, end_date],)
+
+            if prev_study != '-----':
+                qs = qs.filter(protocol=prev_study)
 
         df = read_frame(qs, fieldnames=['study_maternal_identifier', 'protocol'])
 
         # df = df.drop_duplicates(subset=['study_maternal_identifier'])
 
         prev_study_list = []
-        for prev_study in prev_studies:
+        for prev_study in self.prev_studies:
             df_prev = df[df['protocol'] == prev_study]
             prev_study_list.append([prev_study, df_prev[df_prev.columns[0]].count()])
         return prev_study_list
@@ -74,17 +67,6 @@ class PrevStudyRecruitmentReportMixin:
     def cont_contact(self, prev_study=None, start_date=None, end_date=None):
         """Return number of contacted participants who are still being contacted.
         """
-        prev_studies = [
-            'Mpepu',
-            'Mma Bana',
-            'Mashi',
-            'Tshilo Dikotla',
-            'Tshipidi']
-
-        worklist_qs = WorkList.objects.filter(
-            is_called=True,
-            assigned__isnull=False,
-        ).values_list('study_maternal_identifier')
 
         screening_identifiers = SubjectConsent.objects.all().values_list(
             'screening_identifier', flat=True)
@@ -98,8 +80,7 @@ class PrevStudyRecruitmentReportMixin:
             'study_maternal_identifier')
 
         qs = LogEntry.objects.filter(
-            Q(study_maternal_identifier__in=worklist_qs)
-            & ~Q(study_maternal_identifier__in=no_appt_pids)
+            ~Q(study_maternal_identifier__in=no_appt_pids)
             & ~Q(study_maternal_identifier__in=consented_pids),
             ~Q(phone_num_success=['none_of_the_above']),
             appt__in=['thinking', YES])
@@ -119,7 +100,7 @@ class PrevStudyRecruitmentReportMixin:
         result = result.drop_duplicates(subset=['study_maternal_identifier'])
 
         prev_study_list = []
-        for prev_study in prev_studies:
+        for prev_study in self.prev_studies:
             df_prev = result[result['prev_study'] == prev_study]
             prev_study_list.append([prev_study, df_prev[df_prev.columns[0]].count()])
         return prev_study_list
@@ -127,40 +108,33 @@ class PrevStudyRecruitmentReportMixin:
     def attempts(self, prev_study=None, start_date=None, end_date=None):
         """Return number of contacted participants.
         """
-        prev_studies = [
-            'Mpepu',
-            'Mma Bana',
-            'Mashi',
-            'Tshilo Dikotla',
-            'Tshipidi']
 
-        qs = WorkList.objects.filter(
+        worklist_qs = WorkList.objects.filter(
             is_called=True,
             assigned__isnull=False,
             called_datetime__isnull=False
         ).values_list('study_maternal_identifier')
 
         qs = LogEntry.objects.filter(
-            study_maternal_identifier__in=qs,
+            study_maternal_identifier__in=worklist_qs,
         )
 
         if prev_study:
             if prev_study == '-----':
-                qs = LogEntry.objects.filter(
-                    created__range=[start_date, end_date])
 
-                qs = WorkList.objects.filter(
+                worklist_qs = WorkList.objects.filter(
                     is_called=True,
                     assigned__isnull=False,
                     created__range=[start_date, end_date]
                 ).values_list('study_maternal_identifier')
 
                 qs = LogEntry.objects.filter(
-                    study_maternal_identifier__in=qs
+                    study_maternal_identifier__in=worklist_qs,
+                    created__range=[start_date, end_date]
                 )
 
             else:
-                qs = LogEntry.objects.filter(
+                qs = qs.objects.filter(
                     prev_study=prev_study,
                     created__range=[start_date, end_date])
 
@@ -183,7 +157,7 @@ class PrevStudyRecruitmentReportMixin:
         result = result.drop_duplicates(subset=['study_maternal_identifier'])
 
         prev_study_list = []
-        for prev_study in prev_studies:
+        for prev_study in self.prev_studies:
             df_prev = result[result['prev_study'] == prev_study]
             prev_study_list.append([prev_study, df_prev[df_prev.columns[0]].count()])
         return prev_study_list
@@ -191,12 +165,6 @@ class PrevStudyRecruitmentReportMixin:
     def pending(self, prev_study=None, start_date=None, end_date=None):
         """Return number of participants who are randomised but nothing has been done on them.
         """
-        prev_studies = [
-            'Mpepu',
-            'Mma Bana',
-            'Mashi',
-            'Tshilo Dikotla',
-            'Tshipidi']
 
         qs = WorkList.objects.filter(
             assigned__isnull=False)
@@ -215,7 +183,7 @@ class PrevStudyRecruitmentReportMixin:
         df = df.drop_duplicates(subset=['study_maternal_identifier'])
 
         prev_study_list = []
-        for prev_study in prev_studies:
+        for prev_study in self.prev_studies:
             df_prev = df[df['prev_study'] == prev_study]
             prev_study_list.append([prev_study, df_prev[df_prev.columns[0]].count()])
         return prev_study_list
@@ -223,12 +191,7 @@ class PrevStudyRecruitmentReportMixin:
     def non_randomized(self, prev_study=None, start_date=None, end_date=None):
         """Return number of participants who are randomised but nothing has been done on them.
         """
-        prev_studies = [
-            'Mpepu',
-            'Mma Bana',
-            'Mashi',
-            'Tshilo Dikotla',
-            'Tshipidi']
+
         qs = WorkList.objects.filter(
             assigned__isnull=True,
         )
@@ -243,7 +206,7 @@ class PrevStudyRecruitmentReportMixin:
         df = df.drop_duplicates(subset=['study_maternal_identifier'])
 
         prev_study_list = []
-        for prev_study in prev_studies:
+        for prev_study in self.prev_studies:
             df_prev = df[df['prev_study'] == prev_study]
             prev_study_list.append([prev_study, df_prev[df_prev.columns[0]].count()])
         return prev_study_list
@@ -251,12 +214,6 @@ class PrevStudyRecruitmentReportMixin:
     def unable_to_reach(self, prev_study=None, start_date=None, end_date=None):
         """Return number of contacted participants.
         """
-        prev_studies = [
-            'Mpepu',
-            'Mma Bana',
-            'Mashi',
-            'Tshilo Dikotla',
-            'Tshipidi']
 
         # Call logs
         # identifiers = []
@@ -300,7 +257,7 @@ class PrevStudyRecruitmentReportMixin:
         df = df.drop_duplicates(subset=['study_maternal_identifier'])
 
         prev_study_list = []
-        for prev_study in prev_studies:
+        for prev_study in self.prev_studies:
             df_prev = df[df['prev_study'] == prev_study]
             prev_study_list.append([prev_study, df_prev[df_prev.columns[0]].count()])
         return prev_study_list
@@ -308,12 +265,6 @@ class PrevStudyRecruitmentReportMixin:
     def decline_uninterested(self, prev_study=None, start_date=None, end_date=None):
         """Return number of contacted participants.
         """
-        prev_studies = [
-            'Mpepu',
-            'Mma Bana',
-            'Mashi',
-            'Tshilo Dikotla',
-            'Tshipidi']
 
         qs = LogEntry.objects.filter(Q(may_call__iexact='No') | Q(appt__iexact='No'))
 
@@ -359,7 +310,7 @@ class PrevStudyRecruitmentReportMixin:
         df = pd.concat(frames)
 
         prev_study_list = []
-        for prev_study in prev_studies:
+        for prev_study in self.prev_studies:
             df_prev = df[df['prev_study'] == prev_study]
             prev_study_list.append([prev_study, df_prev[df_prev.columns[0]].count()])
         return prev_study_list
@@ -367,12 +318,6 @@ class PrevStudyRecruitmentReportMixin:
     def consented(self, prev_study=None, start_date=None, end_date=None):
         """Return number of contacted participants.
         """
-        prev_studies = [
-            'Mpepu',
-            'Mma Bana',
-            'Mashi',
-            'Tshilo Dikotla',
-            'Tshipidi']
 
         screening_identifiers = SubjectConsent.objects.all().values_list(
             'screening_identifier', flat=True)
@@ -383,7 +328,7 @@ class PrevStudyRecruitmentReportMixin:
         df = df.drop_duplicates(subset=['study_maternal_identifier'])
 
         prev_study_list = []
-        for prev_study in prev_studies:
+        for prev_study in self.prev_studies:
             df_prev = df[df['protocol'] == prev_study]
             prev_study_list.append([prev_study, df_prev[df_prev.columns[0]].count()])
         return prev_study_list
@@ -391,28 +336,20 @@ class PrevStudyRecruitmentReportMixin:
     def thinking(self, prev_study=None, start_date=None, end_date=None):
         """Return number of contacted participants.
         """
-        prev_studies = [
-            'Mpepu',
-            'Mma Bana',
-            'Mashi',
-            'Tshilo Dikotla',
-            'Tshipidi']
+
         qs = LogEntry.objects.filter(appt__iexact='thinking')
+
         if prev_study:
-            if prev_study == '-----':
-                qs = LogEntry.objects.filter(
-                    created__range=[start_date, end_date],
-                    appt__iexact='thinking')
-            else:
-                qs = LogEntry.objects.filter(
-                    prev_study=prev_study,
-                    created__range=[start_date, end_date],
-                    appt__iexact='thinking')
+
+            qs = qs.filter(created__range=[start_date, end_date])
+            if prev_study != '-----':
+                qs = qs.filter(prev_study=prev_study)
+
         df = read_frame(qs, fieldnames=['prev_study', 'study_maternal_identifier'])
         df = df.drop_duplicates(subset=['study_maternal_identifier'])
 
         prev_study_list = []
-        for prev_study in prev_studies:
+        for prev_study in self.prev_studies:
             df_prev = df[df['prev_study'] == prev_study]
             prev_study_list.append([prev_study, df_prev[df_prev.columns[0]].count()])
         return prev_study_list
