4cead567363c617d4ed5f7a8e1fb5742c67b8471
Tshepiso Setsiba <tsetsiba@burs.org.bw>
2022-03-20 22:28:37 +0200

    recruitment report first complete draft

--- flourish_reports/views/recruitment/recruitment_reports.py
+++ flourish_reports/views/recruitment/recruitment_reports.py
@@ -6,13 +6,12 @@ from django.views.generic.base import TemplateView
 from edc_base.view_mixins import EdcBaseViewMixin
 from edc_navbar import NavbarViewMixin
 
-
 from ...classes import RecruitmentReport
+from ...classes.recruitment_reports_classs import PieTotals
 
 
 class RecruitmentReportView(EdcBaseViewMixin,
                             NavbarViewMixin, TemplateView, LoginRequiredMixin):
-
     template_name = 'flourish_reports/recruit/recruitment_reports.html'
     navbar_name = 'flourish_reports'
     navbar_selected_item = 'flourish_reports'
@@ -22,13 +21,49 @@ class RecruitmentReportView(EdcBaseViewMixin,
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        
+
         reports = RecruitmentReport()
 
+        reports.attemps_report_data()
+
+        prev_study_data, pie = reports.caregiver_prev_study_dataset()
+
+        attempts_prev_studies = [
+            'Previous Study',
+            'Total Study Participants',
+            'Total Attempts',
+            'Total not attempted']
+        declined_data, total_decline = reports.declined()
+        consented_data, total_consented = reports.consented()
+        participants_not_reachable, total_participants_not_reachable = reports.participants_not_reachable()
+        participants_to_call_again, total_participants_to_call_again = reports.participants_to_call_again()
+
+        summary_pie = PieTotals(
+            total_continued_contact=total_participants_to_call_again,
+            total_consented=total_consented,
+            total_unable_to_reach=total_participants_not_reachable,
+            total_decline_uninterested=total_decline)
+        attempts_data, total_attempts, not_attempted = reports.attemps_report_data()
         context.update(
-            prev_study_data=reports.caregiver_prev_study_dataset())
+            previous_studies=reports.previous_studies,
+            prev_study_data=prev_study_data,
+            locator_report=reports.locator_report(),
+            pie_chart=pie,
+            worklist_report=reports.worklist_report(),
+            attempts_prev_studies=attempts_prev_studies,
+            attempts_data=attempts_data,
+            total_attempts=total_attempts,
+            not_attempted=not_attempted,
+            participants_to_call_again=participants_to_call_again,
+            participants_not_reachable=participants_not_reachable,
+            declined=declined_data,
+            consented=consented_data,
+            summary_pie=summary_pie
+        )
         return context
 
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
+
+# TODO test again
