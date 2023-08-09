53f2052cd185fe3ff96bc4c3a0682dfd2792ab4c
nmunatsibw <mcturnergarai@gmail.com>
2023-08-09 09:29:20 +0200

    :recycle: refactor(enrolment reports): Update enrolment report code
    Refactor the enrolment reports generation code to improve performance and readability. Changes include replacing pandas DataFrame operations with dictionary-based solutions, simplifying Flask template logic, and restructuring nested functions into a more readable format. Also, add some utility functions for more specific operations like converting string formats and included a method to convert defaultdict instances into regular dictionaries. Overall these changes are expected to improve the code's performance and maintainability.

--- flourish_reports/templatetags/flourish_reports_dashboard_extras.py
+++ flourish_reports/templatetags/flourish_reports_dashboard_extras.py
@@ -6,7 +6,7 @@ register = template.Library()
 @register.inclusion_tag('flourish_reports/enrolment/cohort_breakdown.html')
 def cohort_breakdown(data, title):
     return dict(
-        data=dict(data),
+        data=data,
         title=title
     )
 
@@ -35,5 +35,21 @@ def cohort_c_panel(data):
 
 @register.filter
 def get_item(dictionary, key):
-    if dictionary is not None:
+    if dictionary is not None and isinstance(dictionary, dict):
         return dictionary.get(key)
+
+
+@register.filter
+def get_item_key(dictionary, key):
+    title_to_snake_case_key = title_to_snake_case(key)
+    if dictionary is not None and isinstance(dictionary, dict):
+        return dictionary.get(title_to_snake_case_key)
+
+
+def title_to_snake_case(title):
+    # Replace spaces with underscores
+    snake = title.replace(" ", "_")
+    # Convert all characters to lower case
+    snake = snake.lower()
+    # Return the snake case string
+    return snake
--- flourish_reports/views/enrolment/enrollment_report_mixin.py
+++ flourish_reports/views/enrolment/enrollment_report_mixin.py
@@ -1,13 +1,7 @@
-import re
 from collections import defaultdict
 
 from django.apps import apps as django_apps
 from django.db.models import Max
-
-
-def convert_to_snake_case(string):
-    snake_case_string = re.sub(r'\s', '_', string.lower())
-    return snake_case_string
 
 
 def convert_to_title_case(snake_case_string):
@@ -16,153 +10,73 @@ def convert_to_title_case(snake_case_string):
 
 
 class EnrolmentReportMixin:
-    child_consents_cls = django_apps.get_model('flourish_caregiver.caregiverchildconsent')
-    maternal_dataset_cls = django_apps.get_model('flourish_caregiver.maternaldataset')
     cohort_cls = django_apps.get_model('flourish_caregiver.cohort')
-    child_dataset_cls = django_apps.get_model('flourish_child.childdataset')
-    child_birth_cls = django_apps.get_model('flourish_child.childbirth')
-    ante_enrol_cls = django_apps.get_model('flourish_caregiver.antenatalenrollment')
 
     @property
     def participants_cohort(self):
         return self.cohort_cls.objects.all()
 
-    @property
-    def latest_cohort_objs(self):
-        latest_cohort_objs_ids = self.participants_cohort.values(
-            'subject_identifier').annotate(latest_report_date=Max(
-            'assign_datetime')).values_list('id', flat=True)
-
-        return self.participants_cohort.filter(id__in=latest_cohort_objs_ids)
+    def get_cohorts(self, enroll=False):
+        if enroll:
+            return self.participants_cohort.filter(enrollment_cohort=True)
+        else:
+            return self.participants_cohort.all()
 
     @property
-    def child_consent_objs(self):
-        return self.child_consents_cls.objects.all()
+    def get_sequence(self):
+        participant_cohorts = defaultdict(list)
+        all_cohorts = self.participants_cohort.order_by('assign_datetime')
 
-    @property
-    def child_birth_objs(self):
-        return self.child_birth_cls.objects.all()
+        for cohort in all_cohorts:
+            participant_cohorts[cohort.subject_identifier].append(cohort.name)
 
-    def get_enrolment_by_cohort(self, cohort, enrollment_cohort=True):
-        return self.participants_cohort.filter(
-            name=cohort, enrollment_cohort=enrollment_cohort).values_list(
-            'subject_identifier', flat=True)
+        movements = defaultdict(lambda: defaultdict(int))
 
-    def get_current_by_cohort(self, cohort):
-        enrolment_cohort = self.latest_cohort_objs.filter(
-            name=cohort).values_list('subject_identifier', flat=True)
-        return set(list(enrolment_cohort))
+        for cohorts in participant_cohorts.values():
+            for i in range(1, len(cohorts)):
+                # increment count of movement from previous_cohort to next_cohort
+                movements[cohorts[i - 1]][cohorts[i]] += 1
 
-    def get_seq_by_cohort(self, enrol_cohort, seq_cohort):
-        enrolment_cohort = self.get_enrolment_by_cohort(enrol_cohort)
-        seq_by_cohort = []
-        for participant in enrolment_cohort:
-            participants_cohort = self.participants_cohort.filter(
-                subject_identifier=participant, name=seq_cohort)
-            if participants_cohort.exists():
-                seq_by_cohort.append(participant)
+        return movements
 
-        return set(list(seq_by_cohort))
-
-    def all_cohort_report(self, start_date=None, end_date=None):
-        """Return a total enrolment per cohort.
-        """
-        table = {
-            'Cohort A': ['enrollment', 'cohort_b', 'cohort_b_sec', 'cohort_c',
-                         'cohort_c_sec'],
-            'Cohort B': ['enrollment', 'cohort_b', 'cohort_b_sec', 'cohort_c',
-                         'cohort_c_sec'],
-            'Cohort C': ['enrollment', 'cohort_b', 'cohort_b_sec', 'cohort_c',
-                         'cohort_c_sec'],
-            'Cohort A Sec': ['enrollment', 'cohort_b', 'cohort_b_sec', 'cohort_c',
-                             'cohort_c_sec'],
-            'Cohort B Sec': ['enrollment', 'cohort_b', 'cohort_b_sec', 'cohort_c',
-                             'cohort_c_sec'],
-            'Cohort C Sec': ['enrollment', 'cohort_b', 'cohort_b_sec', 'cohort_c',
-                             'cohort_c_sec'],
-        }
-        cohort_report = {}
-        for cohort, values in table.items():
-            cohort_to_snake_case = convert_to_snake_case(cohort)
-            cohort_report[cohort] = {'enrollment': self.get_enrolment_by_cohort(
-                cohort_to_snake_case).count()}
-            for value in values[1:]:
-                if value == cohort_to_snake_case or cohort_to_snake_case in value or \
-                        value in cohort_to_snake_case:
-                    cohort_report[cohort][value] = 0
-                    continue
-                cohort_report[cohort][value] = len(self.get_seq_by_cohort(
-                    enrol_cohort=cohort_to_snake_case, seq_cohort=value))
-        return cohort_report
-
-    @property
-    def enrolment_exposure_summary(self):
+    def generate_report(self, enroll=False, schedule_status=False):
         cohorts = ['cohort_a', 'cohort_b', 'cohort_c', 'cohort_a_sec', 'cohort_b_sec',
                    'cohort_c_sec']
-        enrolment_exposure_summary = defaultdict(lambda: defaultdict(int))
 
-        for cohort in cohorts:
-            participants = self.get_enrolment_by_cohort(cohort=cohort)
-            cohort_name = convert_to_title_case(cohort)
-            for participant in participants:
-                exposure = self.check_exposure(participant)
-                enrolment_exposure_summary[cohort_name][exposure] += 1
-                if participant[:-3] in self.heu_art_3drug_combination:
-                    enrolment_exposure_summary[cohort_name]['3drug'] += 1
-        return enrolment_exposure_summary
+        report = []
+        all_cohorts = self.get_cohorts(enroll)
+        if schedule_status:
+            all_cohorts = all_cohorts.filter(schedule_status=schedule_status)
 
-    @property
-    def current_exposure_summary(self):
-        current_exposure_summary = defaultdict(lambda: defaultdict(int))
+        for cohort_name in cohorts:
+            title_case_cohort_name = convert_to_title_case(cohort_name)
+            exposed = all_cohorts.filter(
+                name=cohort_name, exposure_status='EXPOSED').values_list(
+                'subject_identifier', flat=True).distinct()
+            unexposed = all_cohorts.filter(
+                name=cohort_name, exposure_status='UNEXPOSED').values_list(
+                'subject_identifier', flat=True).distinct()
+            exposed = len(set(exposed))
+            unexposed = len(set(unexposed))
 
-        participants = self.child_consent_objs.only('subject_identifier').values_list(
-            'subject_identifier', flat=True)
-        for participant in set(list(participants)):
-            try:
-                latest_obj = self.participants_cohort.filter(
-                    subject_identifier=participant).latest('assign_datetime')
-            except self.cohort_cls.DoesNotExist:
-                continue
-            else:
-                exposure = self.check_exposure(participant)
-                cohort_name = convert_to_title_case(latest_obj.name)
-                current_exposure_summary[cohort_name][exposure] += 1
-                if participant[:-3] in self.heu_art_3drug_combination:
-                    current_exposure_summary[cohort_name]['3drug'] += 1
-        return current_exposure_summary
+            report.append(
+                {'cohort_name': title_case_cohort_name, 'unexposed': unexposed,
+                 'exposed': exposed})
 
-    def check_exposure(self, participant):
-        study_child_identifier = self.child_consent_objs.filter(
-            subject_identifier=participant).only('study_child_identifier').latest(
-            'consent_datetime').study_child_identifier
-        if study_child_identifier in self.unexposed_participants:
-            return 'Unexposed'
-        elif study_child_identifier in self.exposed_participants:
-            return 'Exposed'
-        else:
-            return 'ANC'
+        return report
 
     @property
-    def exposed_participants(self):
-        exposed_values = ['Exposed', 'exposed']
-        return self.child_dataset_cls.objects.filter(
-            infant_hiv_exposed__in=exposed_values).values_list(
-            'study_child_identifier', flat=True)
+    def current_report(self):
+        return self.generate_report(enroll=False, schedule_status=True)
 
     @property
-    def heu_art_3drug_combination(self):
-        exposed_values = ['Exposed', 'exposed']
-        exposed_participants = self.child_dataset_cls.objects.filter(
-            infant_hiv_exposed__in=exposed_values).values_list(
-            'study_maternal_identifier', flat=True)
-
-        return self.maternal_dataset_cls.objects.filter(
-            study_maternal_identifier__in=exposed_participants,
-            mom_pregarv_strat='3-drug ART').values_list('subject_identifier', flat=True)
+    def enrollment_report(self):
+        return self.generate_report(enroll=True)
 
     @property
-    def unexposed_participants(self):
-        unexposed_values = ['Unexposed', 'unexposed']
-        return self.child_dataset_cls.objects.filter(
-            infant_hiv_exposed__in=unexposed_values).values_list(
-            'study_child_identifier', flat=True)
+    def get_enrolment_total(self):
+        enrolment_total = {}
+        for cohort in self.enrollment_report:
+            enrolment_total[cohort['cohort_name']] = cohort['unexposed'] + cohort[
+                'exposed']
+        return enrolment_total
